978b92f28ccdb4c9d81eed3b341154b3c2542d2e
jitongzou <jtzou0430@gmail.com>
2024-09-09 09:59:04 -0400

    update

--- client.py
+++ client.py
@@ -1,19 +1,23 @@
 #Jitong Zou
 #CS5700
-
 import socket
 import sys
 
-HOST = "127.0.0.1" #The server's host name or IP address
-PORT = 65432 #The port used by the server
+# Set up server address and port
+HOST = "127.0.0.1" 
+PORT = 65432 
 
+# Retrieve command-line arguments
 args = sys.argv
-num = args[1]
+num = args[1] # Get the number passed to the script
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #Create a new socket using teh given address famiily, socket
-    #type and protocol number
-    s.connect((HOST, PORT)) #Connect to a remote socket at the address and port
-    #is byte
-    data = s.recv(1024) #Receive data from the socket. The return type is a bytes object representing the data received.
-    #The maximum amount of data to be received in each chunk is 1024 as specified.
+# Create a TCP/IP socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+    # Connect to the server
+    s.connect((HOST, PORT)) 
+    # Send the number to the server, encoded to bytes
+    s.sendall(num.encode())
+    # Receive the response from the server, limited to 1024 bytes
+    data = s.recv(1024) 
+    # Print out what was sent and received
     print(f"Sent {num} and received {data!r}") #print to console
