baf9af4a4e9392b7ad9a98fdd2121c0ab847bc40
jitongzou <jtzou0430@gmail.com>
2024-09-09 09:59:22 -0400

    update

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
 #Jitong Zou
 #CS5700
-
 import socket
 
-HOST = "127.0.0.1" #Standard loopback interface address (localhost)
-PORT = 65432 #Port to listen on
+# Set up server address and port
+HOST = "127.0.0.1" 
+PORT = 65432 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #Create a new socket using the given address family,
-    # socket type, and protocol number
-    s.bind((HOST, PORT)) #Bind the socket to an address and port number. The socket must not already be bound.
-    s.listen() #Enable a server to accept connections.
-    conn, addr = s.accept() #Accept a connection. The socket must be bound to an address and listening for connections.
-    #The return value is a pair (conn, address) where conn is a new socket object usable to send and receive data on
-    #the connection, and address is the address bound to the socket on the other end of the connection.
-    with conn: #with keyword is used for unmanaged resources such as socket stream or file stream, used for exception
-        #handling
-        while True: #looping the input stream to receive all byte data from the client
+#Create a new socket using the given address family,
+#socket type, and protocol number
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+    #Bind the socket to an address and port number.
+    s.bind((HOST, PORT)) 
+    #Enable a server to accept connections.
+    s.listen() 
+    conn, addr = s.accept() #
+    #with keyword is used for unmanaged resources such as socket stream or file stream, used for exception handling
+    with conn: 
+        while True: 
             data = conn.recv(1024) #receives the data from the client
             if not data:
                 break #breaks out once no more data to receive
             data = int(data) + 100
-            conn.sendall(str(data).encode()) #send the data back to the client (echo it back). The socket must be connected to a remote
-            #socket (client's socket).
+            conn.sendall(str(data).encode()) #send the data back to the client. The socket must be connected to a remote socket (client's socket).
