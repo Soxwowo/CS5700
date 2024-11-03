d4a8544beee5adb994e7f8cf137d4aea9e21fdc8
jitongzou <jtzou0430@gmail.com>
2024-11-03 15:22:33 -0500

    update

--- hw8/salesforce_solution.py
+++ hw8/salesforce_solution.py
@@ -10,27 +10,15 @@ import json
 config = configparser.ConfigParser()
 config.read('salesforceconfig.ini')
 
-# Retrieve OAuth2 information from the configuration file
-grant_type = config.get('OAUTH', 'grant_type')
-client_id = config.get('OAUTH', 'client_id')
-client_secret = config.get('OAUTH', 'client_secret')
-username = config.get('OAUTH', 'username')
-password = config.get('OAUTH', 'password')
-security_token = config.get('OAUTH', 'security_token')
-base_url = config.get('OAUTH', 'base_url')
-
-# Splicing passwords for authentication
-full_password = password + security_token
-
 with requests.Session() as session:
-    # Request URL and data
-    auth_url = f"{base_url}/services/oauth2/token"
+    # OAuth2 request URL and data
+    auth_url = f"{config.get('OAUTH', 'base_url')}/services/oauth2/token"
     auth_data = {
-        'grant_type': grant_type,
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'username': username,
-        'password': full_password
+        'grant_type': config.get('OAUTH', 'grant_type'),
+        'client_id': config.get('OAUTH', 'client_id'),
+        'client_secret': config.get('OAUTH', 'client_secret'),
+        'username': config.get('OAUTH', 'username'),
+        'password': config.get('OAUTH', 'password') + config.get('OAUTH', 'security_token')  # Directly concatenate password and security token
     }
 
     try:
@@ -45,11 +33,12 @@ with requests.Session() as session:
             'Content-Type': 'application/json'
         }
 
-        # Request Account Data
-        account_url = f"{base_url}/services/data/v55.0/query/?q=SELECT+NAME+,+ID+,+BillingAddress+FROM+ACCOUNT"
+        # Request Account data
+        account_url = f"{config.get('OAUTH', 'base_url')}/services/data/v55.0/query/?q=SELECT+NAME+,+ID+,+BillingAddress+FROM+ACCOUNT"
         account_response = session.get(account_url, headers=headers)
         account_response.raise_for_status()
 
+        # Output byte stream
         byte_stream = account_response.content
         print(byte_stream)
     except requests.exceptions.HTTPError as err:
